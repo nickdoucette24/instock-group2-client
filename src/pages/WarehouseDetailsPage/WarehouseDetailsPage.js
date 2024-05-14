b3c5326aa914da7190957c9587012c83bda63c6a
Alex <alexclarkson9@yahoo.com>
2024-05-14 19:58:02 -0400

    add icons to front-end assets

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.js
@@ -1,7 +1,28 @@
 import "./WarehouseDetailsPage.scss";
 
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const WarehouseDetailsPage = () => {
-  return <div>WarehouseDetailsPage</div>;
+	return (
+		<div>
+			<main>
+				<div className="main-header">
+					<button className="back-btn"></button>
+					<h1 className="warehouse-name"></h1>
+					<button className="edit-btn">
+						<img />
+						Edit
+					</button>
+				</div>
+				<div className="warehouse-info">
+					<div className="warehouse-info__address"></div>
+					<div className="warehouse-info__contact"></div>
+				</div>
+				{/* <div>inventory list</div> */}
+			</main>
+		</div>
+	);
 };
 
 export default WarehouseDetailsPage;
