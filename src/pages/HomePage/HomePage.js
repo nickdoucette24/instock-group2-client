8b80b5bd9255d3782aedf68e15e3904d6fc58f6a
Kevin Meisenbacher <BeastOfTheEast23@hotmail.ca>
2024-05-13 20:41:43 -0400

    Got the homepage laid out nicely

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import "./HomePage.scss";
-import searchIcon from "../../assets/Icons/search-24px.svg"
-import chevron from "../../assets/Icons/chevron_right-24px.svg"
+import searchIcon from "../../assets/Icons/search-24px.svg";
+import chevron from "../../assets/Icons/chevron_right-24px.svg";
+import edit from "../../assets/Icons/edit-24px.svg";
+import del from "../../assets/Icons/delete_outline-24px.svg";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -13,10 +15,10 @@ const HomePage = () => {
     axios.get('http://localhost:8080/warehouses')
     .then(response => {
       setWarehouses(response.data);
+      setLoading(false);
+      console.log(warehouses);
       return warehouses;
     })
-    .then(setLoading(false))
-    .then(console.log(warehouses))
     .catch(err => console.error(err));
   }, []);
 
@@ -26,30 +28,36 @@ const HomePage = () => {
         <h1>Warehouses</h1>
         <input className="search" type="text" placeholder="Search..." src={searchIcon}/>
         <button className="btn__add">+ Add New Warehouse</button>
-      {warehouses.map((warehouse) => {
-        <section>
-        <div>
-          <h4>WAREHOUSE</h4>
-          <span>
-          <h3>{warehouse.warehouse_name}</h3>
-          <img src={chevron} alt="" />
-          </span>
-        </div>
-          <div>
-            <h4>CONTACT NAME</h4>
-            <p>{warehouse.contact_name}</p>
-          </div>
-          <div>
-            <h4>ADDRESS</h4>
-            <p>{warehouse.address}</p>
-          </div>
-          <div>
+      {warehouses.map(warehouse => {
+        return(
+          <section className="warehouse-container">
+          <div class="warehouse-container__item">
             <h4>WAREHOUSE</h4>
-            <h4>CONTACT INFORMATION</h4>
-            <p>{warehouse.contact_phone}</p>
-            <p>{warehouse.contact_email}</p>
+            <span>
+              <h3>{warehouse.warehouse_name}</h3>
+              <img src={chevron} alt="" />
+            </span>
           </div>
-        </section>
+            <div class="warehouse-container__item">
+              <h4>CONTACT NAME</h4>
+              <p className="p2">{warehouse.contact_name}</p>
+            </div>
+            <div class="warehouse-container__item">
+              <h4>ADDRESS</h4>
+              <p className="p2">{warehouse.address}</p>
+            </div>
+            <div class="warehouse-container__item">
+              <h4>CONTACT INFORMATION</h4>
+              <p className="p2">{warehouse.contact_phone}</p>
+              <p className="p2">{warehouse.contact_email}</p>
+            </div>
+            <div className="options">
+              <img src={edit} alt="Edit" />
+              <img src={del} alt="Delete" />
+            </div>
+            <span className="divider"></span>
+          </section>
+        )
       })}
       </div>
     </main>
