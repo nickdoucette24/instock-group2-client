b2fdb967858e0e7f6e5d5caed8778f9afbaea3c9
Alex <alexclarkson9@yahoo.com>
2024-05-14 20:15:55 -0400

    add Header and Footer components to App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "App.scss";
 import Header from "./components/Header/Header/";
 import Footer from "./components/Footer/Footer/";
 
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/warehouses/:id" element={<WarehouseDetailsPage />} />
-        <Route path="/warehouses/:id/edit" element={<AddEditWarehousePage />} />
-        <Route path="/warehouses/add" element={<AddEditWarehousePage />} />
-        <Route path="/inventories" element={<InventoryPage />} />
-        <Route path="/inventories/:id" element={<InventoryDetailsPage />} />
-        <Route
-          path="/inventories/:id/edit"
-          element={<AddEditInventoryPage />}
-        />
-        <Route path="/inventories/add" element={<AddEditInventoryPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/warehouses/:id" element={<WarehouseDetailsPage />} />
+				<Route path="/warehouses/:id/edit" element={<AddEditWarehousePage />} />
+				<Route path="/warehouses/add" element={<AddEditWarehousePage />} />
+				<Route path="/inventories" element={<InventoryPage />} />
+				<Route path="/inventories/:id" element={<InventoryDetailsPage />} />
+				<Route
+					path="/inventories/:id/edit"
+					element={<AddEditInventoryPage />}
+				/>
+				<Route path="/inventories/add" element={<AddEditInventoryPage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
+	);
 }
 
 export default App;
