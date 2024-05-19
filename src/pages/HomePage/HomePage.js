2afbd7bd41e87a7506748efa6e4d4a9e9b8c0a89
Kevin Meisenbacher <BeastOfTheEast23@hotmail.ca>
2024-05-19 19:41:49 -0400

    Completed homepage as far as I know

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import "./HomePage.scss";
-import searchIcon from "../../assets/Icons/search-24px.svg";
 import chevron from "../../assets/Icons/chevron_right-24px.svg";
 import edit from "../../assets/Icons/edit-24px.svg";
 import del from "../../assets/Icons/delete_outline-24px.svg";
@@ -18,11 +17,15 @@ const HomePage = () => {
     .then(response => {
       setWarehouses(response.data);
       setLoading(false);
-      console.log(warehouses);
       return warehouses;
     })
     .catch(err => console.error(err));
   }, []);
+
+  // TODO: Add sorting functionality
+  const handleSort = () => {
+    warehouses.sort();
+  }
 
   if (!loading) return (
   <>
@@ -30,27 +33,29 @@ const HomePage = () => {
       <div className="floaty-container">
         <div className="floaty-container__top">
           <h1>Warehouses</h1>
-          <span className="floaty-container__top--inputs">
-            <input className="search" type="text" placeholder="Search..." src={searchIcon}/>
-            <button className="btn__add">+ Add New Warehouse</button>
+          <span className="floaty-container__top--controls">
+            <input className="search" type="text" placeholder="Search..."/>
+            <Link to="/warehouses/add"> {/* Add new warehouse */}
+              <button className="btn__add">+ Add New Warehouse</button>
+            </Link>
           </span>
         </div>
         <div className="sorty-selectors">
           <span className="sorty-selectors__item">
           <h4>WAREHOUSE</h4>
-            <img src={sortBtn} alt="sort" />
+            <img src={sortBtn} onClick={handleSort()} alt="sort" />
           </span>
           <span className="sorty-selectors__item">
           <h4>CONTACT NAME</h4>
-            <img src={sortBtn} alt="sort" />
+            <img src={sortBtn} onClick={handleSort()} alt="sort" />
           </span>
           <span className="sorty-selectors__item">
           <h4>ADDRESS</h4>
-            <img src={sortBtn} alt="sort" />
+            <img src={sortBtn} onClick={handleSort()} alt="sort" />
           </span>
           <span className="sorty-selectors__item">
           <h4>CONTACT INFORMATION</h4>
-            <img src={sortBtn} alt="sort" />
+            <img src={sortBtn} onClick={handleSort()} alt="sort" />
           </span>
           <span className="sorty-selectors__item col5">
           <h4>ACTIONS</h4>
@@ -58,37 +63,43 @@ const HomePage = () => {
         </div>
       {warehouses.map(warehouse => {
         return(
-          <section className="warehouse-container">
-            <div class="warehouse-container__item">
-              <h4 className="mobile">WAREHOUSE</h4>
-              <span>
-                <Link to={`/warehouses/${warehouse.id}`}>
-                  <h3>{warehouse.warehouse_name}</h3>
-                </Link>
-                <img src={chevron} alt="" />
-              </span>
-            </div>
+          <>
+            <section className="warehouse-container">
               <div class="warehouse-container__item">
-                <h4 className="mobile">CONTACT NAME</h4>
-                <p className="p2">{warehouse.contact_name}</p>
+                <h4 className="mobile">WAREHOUSE</h4>
+                <span>
+                  <Link to={`/warehouses/${warehouse.id}`}>
+                    <h3>{warehouse.warehouse_name}</h3>
+                  </Link>
+                  <img src={chevron} alt="" />
+                </span>
               </div>
-              <div class="warehouse-container__item">
-                <h4 className="mobile">ADDRESS</h4>
-                <p className="p2">
-                  {warehouse.address}, {warehouse.city}, {warehouse.country}
-                </p>
-              </div>
-              <div class="warehouse-container__item">
-                <h4 className="mobile">CONTACT INFORMATION</h4>
-                <p className="p2">{warehouse.contact_phone}</p>
-                <p className="p2">{warehouse.contact_email}</p>
-              </div>
-              <div className="warehouse-container__item--col5">
-                <img src={del} alt="Delete" />
-                <img src={edit} alt="Edit" />
-              </div>
+                <div class="warehouse-container__item">
+                  <h4 className="mobile">CONTACT NAME</h4>
+                  <p className="p2">{warehouse.contact_name}</p>
+                </div>
+                <div class="warehouse-container__item">
+                  <h4 className="mobile">ADDRESS</h4>
+                  <p className="p2">
+                    {warehouse.address}, {warehouse.city}, {warehouse.country}
+                  </p>
+                </div>
+                <div class="warehouse-container__item">
+                  <h4 className="mobile">CONTACT INFORMATION</h4>
+                  <p className="p2">{warehouse.contact_phone}</p>
+                  <p className="p2">{warehouse.contact_email}</p>
+                </div>
+                <div className="warehouse-container__item--col5">
+                  <Link to="/warehouses/delete/:id">
+                    <img src={del} alt="Delete" />
+                  </Link>
+                  <Link to="/warehouses/edit/:id">
+                    <img src={edit} alt="Edit" />
+                  </Link>
+                </div>
+              </section>
               <span className="divider"></span>
-            </section>
+            </>
         )
       })}
       </div>
