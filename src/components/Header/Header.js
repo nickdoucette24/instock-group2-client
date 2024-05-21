e460587138454119f14ed875e8e9f20487a40664
Nick Doucette <nickdoucette24@gmail.com>
2024-05-20 20:49:47 -0400

    Started header fixes. Pulling from develop - reason for push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,29 @@
-import axios from "axios";
+import logo from '../../assets/Logo/InStock-Logo.svg'
+
 import "./Header.scss";
 
 const Header = () => {
-  return <div className="header__wrapper">
-    <div className="logo__wrapper">
-      <img url="" alt=""></img>
-      <a href="" className="header__logo">instock</a>
-    </div>
-    <div className="tab__wrapper">
-      <button className="btn" placeholder="Warehouses"></button>
-      <button className="btn" placeholder="Inventory"></button>
-    </div>
-  </div>;
+  return (
+    <section className='header'>
+      <div className='header__wrapper'>
+        <img className='header__logo' src={logo} />
+        <div className='header__nav'>
+          <button className='header__nav-btn'>Warehouses</button>
+          <button className='header__nav-btn'>Inventory</button>
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Header;
+
+  // return <div className="header__wrapper">
+  //   <div className="logo__wrapper">
+  //     <img className='logo__wrapper--logoMobile' href={logoMobile} alt="instock logo"></img>
+  //   </div>
+  //   <div className="tab__wrapper">
+  //     <button className="btn" placeholder="Warehouses"></button>
+  //     <button className="btn" placeholder="Inventory"></button>
+  //   </div>
+  // </div>;
