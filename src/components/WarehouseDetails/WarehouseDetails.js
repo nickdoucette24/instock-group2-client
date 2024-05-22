d2f86dd1f337501a4adfa686d638fd3ed41ce36c
Alex <alexclarkson9@yahoo.com>
2024-05-22 17:20:52 -0400

    add ternary to delay loadingcomponent until warehouseDetails state is filled

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -35,83 +35,91 @@ const WarehouseDetails = () => {
 
 	return (
 		<>
-			<div className="main-header">
-				<Link to={"/"} className="back-button-container">
-					<BackButton />
-				</Link>
-				<h1 className="warehouse-name">{warehouseDetails.warehouse_name}</h1>
-				<Link to={`/warehouses/${warehouseDetails.id}/edit`}>
-					<EditButton />
-				</Link>
-			</div>
-			<div className="warehouse-info">
-				<div className="warehouse-info__address">
-					<h4 className="info-header">warehouse address:</h4>
-					<div className="warehouse-info__address-content">
-						<p>{warehouseDetails.address},</p>
-						<p>
-							{warehouseDetails.city}, {warehouseDetails.country}
-						</p>
+			{warehouseDetails ? (
+				<>
+					<div className="main-header">
+						<Link to={"/"} className="back-button-container">
+							<BackButton />
+						</Link>
+						<h1 className="warehouse-name">
+							{warehouseDetails.warehouse_name}
+						</h1>
+						<Link to={`/warehouses/${warehouseDetails.id}/edit`}>
+							<EditButton />
+						</Link>
 					</div>
-				</div>
-				<div className="warehouse-info__contact">
-					<div className="contact-name">
-						<h4 className="info-header">contact name:</h4>
-						<p className="contact-name__name">
-							{warehouseDetails.contact_name}
-						</p>
-						<p className="contact-name__position">
-							{warehouseDetails.contact_position}
-						</p>
+					<div className="warehouse-info">
+						<div className="warehouse-info__address">
+							<h4 className="info-header">warehouse address:</h4>
+							<div className="warehouse-info__address-content">
+								<p>{warehouseDetails.address},</p>
+								<p>
+									{warehouseDetails.city}, {warehouseDetails.country}
+								</p>
+							</div>
+						</div>
+						<div className="warehouse-info__contact">
+							<div className="contact-name">
+								<h4 className="info-header">contact name:</h4>
+								<p className="contact-name__name">
+									{warehouseDetails.contact_name}
+								</p>
+								<p className="contact-name__position">
+									{warehouseDetails.contact_position}
+								</p>
+							</div>
+							<div className="contact-info">
+								<h4 className="info-header">contact information:</h4>
+								<p className="contact-info__phone">
+									{warehouseDetails.contact_phone}
+								</p>
+								<p className="contact-info__email">
+									{warehouseDetails.contact_email}
+								</p>
+							</div>
+						</div>
 					</div>
-					<div className="contact-info">
-						<h4 className="info-header">contact information:</h4>
-						<p className="contact-info__phone">
-							{warehouseDetails.contact_phone}
-						</p>
-						<p className="contact-info__email">
-							{warehouseDetails.contact_email}
-						</p>
+					<div className="warehouse-sort-bar">
+						<div className="warehouse-sort-bar__column">
+							<h4 className="table-header">inventory item</h4>
+							<img
+								className="sort-button"
+								src={sortButton}
+								alt="sort button icon"
+							/>
+						</div>
+						<div className="warehouse-sort-bar__column">
+							<h4 className="table-header">category</h4>
+							<img
+								className="sort-button"
+								src={sortButton}
+								alt="sort button icon"
+							/>
+						</div>
+						<div className="warehouse-sort-bar__column warehouse-sort-bar__column--status">
+							<h4 className="table-header">status</h4>
+							<img
+								className="sort-button"
+								src={sortButton}
+								alt="sort button icon"
+							/>
+						</div>
+						<div className="warehouse-sort-bar__column">
+							<h4 className="table-header">quantity</h4>
+							<img
+								className="sort-button"
+								src={sortButton}
+								alt="sort button icon"
+							/>
+						</div>
+						<div className="warehouse-sort-bar__column">
+							<h4 className="table-header">actions</h4>
+						</div>
 					</div>
-				</div>
-			</div>
-			<div className="warehouse-sort-bar">
-				<div className="warehouse-sort-bar__column">
-					<h4 className="table-header">inventory item</h4>
-					<img
-						className="sort-button"
-						src={sortButton}
-						alt="sort button icon"
-					/>
-				</div>
-				<div className="warehouse-sort-bar__column">
-					<h4 className="table-header">category</h4>
-					<img
-						className="sort-button"
-						src={sortButton}
-						alt="sort button icon"
-					/>
-				</div>
-				<div className="warehouse-sort-bar__column warehouse-sort-bar__column--status">
-					<h4 className="table-header">status</h4>
-					<img
-						className="sort-button"
-						src={sortButton}
-						alt="sort button icon"
-					/>
-				</div>
-				<div className="warehouse-sort-bar__column">
-					<h4 className="table-header">quantity</h4>
-					<img
-						className="sort-button"
-						src={sortButton}
-						alt="sort button icon"
-					/>
-				</div>
-				<div className="warehouse-sort-bar__column">
-					<h4 className="table-header">actions</h4>
-				</div>
-			</div>
+				</>
+			) : (
+				"Loading..."
+			)}
 			{/* inventory list */}
 		</>
 	);
