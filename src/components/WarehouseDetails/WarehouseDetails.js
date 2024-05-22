ab982ef6c3552b172fda74b54ff4c533861e659e
Alex <alexclarkson9@yahoo.com>
2024-05-21 21:43:10 -0400

    add first attempt logic to conditionally render warehouse components based on url

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
-import EditButton from "./EditButton/EditButton";
-import BackButton from "./BackButton/BackButton";
+import EditButton from "../EditButton/EditButton";
+import BackButton from "../BackButton/BackButton";
 
 const WarehouseDetails = () => {
 	const [warehouseDetails, setwarehouseDetails] = useState({});
@@ -22,49 +22,47 @@ const WarehouseDetails = () => {
 	console.log(warehouseDetails);
 
 	return (
-		<main>
-			<div className="floaty-container">
-				<div className="main-header">
-					<Link to={"/"} className="back-button-container">
-						<BackButton />
-					</Link>
-					<h1 className="warehouse-name">{warehouseDetails.warehouse_name}</h1>
-					<EditButton />
-				</div>
-				<div className="warehouse-info">
-					<div className="warehouse-info__address">
-						<h4 className="table-header">warehouse address:</h4>
-						<div className="warehouse-info__address-content">
-							<p>{warehouseDetails.address},</p>
-							<p>
-								{warehouseDetails.city}, {warehouseDetails.country}
-							</p>
-						</div>
-					</div>
-					<div className="warehouse-info__contact">
-						<div className="contact-name">
-							<h4 className="table-header">contact name:</h4>
-							<p className="contact-name__name">
-								{warehouseDetails.contact_name}
-							</p>
-							<p className="contact-name__position">
-								{warehouseDetails.contact_position}
-							</p>
-						</div>
-						<div className="contact-info">
-							<h4 className="table-header">contact information:</h4>
-							<p className="contact-info__phone">
-								{warehouseDetails.contact_phone}
-							</p>
-							<p className="contact-info__email">
-								{warehouseDetails.contact_email}
-							</p>
-						</div>
-					</div>
-				</div>
-				{/* <div>inventory list</div> */}
+		<>
+			<div className="main-header">
+				<Link to={"/"} className="back-button-container">
+					<BackButton />
+				</Link>
+				<h1 className="warehouse-name">{warehouseDetails.warehouse_name}</h1>
+				<EditButton />
 			</div>
-		</main>
+			<div className="warehouse-info">
+				<div className="warehouse-info__address">
+					<h4 className="table-header">warehouse address:</h4>
+					<div className="warehouse-info__address-content">
+						<p>{warehouseDetails.address},</p>
+						<p>
+							{warehouseDetails.city}, {warehouseDetails.country}
+						</p>
+					</div>
+				</div>
+				<div className="warehouse-info__contact">
+					<div className="contact-name">
+						<h4 className="table-header">contact name:</h4>
+						<p className="contact-name__name">
+							{warehouseDetails.contact_name}
+						</p>
+						<p className="contact-name__position">
+							{warehouseDetails.contact_position}
+						</p>
+					</div>
+					<div className="contact-info">
+						<h4 className="table-header">contact information:</h4>
+						<p className="contact-info__phone">
+							{warehouseDetails.contact_phone}
+						</p>
+						<p className="contact-info__email">
+							{warehouseDetails.contact_email}
+						</p>
+					</div>
+				</div>
+			</div>
+			{/* <div>inventory list</div> */}
+		</>
 	);
 };
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,28 @@
 import "./HomePage.scss";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+
+import { useParams } from "react-router-dom";
 
 const HomePage = () => {
+	const urlParams = useParams();
+	console.log(urlParams);
+
 	return (
 		<>
 			<main>
 				<div className="floaty-container">
-					<WarehouseList />
+					{() => {
+						switch (urlParams) {
+							case urlParams.warehouses && urlParams.id:
+								return <WarehouseDetails />;
+
+							default:
+								return <WarehouseList />;
+						}
+					}}
+					{/* <WarehouseDetails /> */}
+					{/* <WarehouseList /> */}
 				</div>
 			</main>
 		</>
