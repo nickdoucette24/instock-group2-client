8b1c406078f3480b18ccdb98356ef3aba76706f4
Alex <alexclarkson9@yahoo.com>
2024-05-26 17:37:11 -0400

    commit to switch branches

--- src/components/DeleteInventoryModal/DeleteInventoryModal.js
+++ src/components/DeleteInventoryModal/DeleteInventoryModal.js
@@ -19,14 +19,14 @@ const DeleteInventoryModal = forwardRef(({item, id, toggleModal}, ref) => {
       }}
     >
       <div className='modal-content-wrapper'>
-        <button type='button' onClick={toggleModal}>
-          <img className='close-icon' src={closeIcon} alt='close icon'/>
+        <button className='close-icon' type='button' onClick={toggleModal}>
+          <img src={closeIcon} alt='close icon'/>
         </button>
         <div className='modal-content'>
           <h1>Delete {item} inventory item</h1>
           <div className='delete-modal-message'>
-            <p className='p1'>Please confirm that you'd like to delete {item} from the inventory list.</p>
-            <p className='p1'>You won't be able to undo this action.</p>
+            <p className='p1 modal-message'>Please confirm that you'd like to delete {item} from the inventory list.</p>
+            <p className='p1 modal-message'>You won't be able to undo this action.</p>
           </div>
           <div className='modal-buttons'>
             <button className="cancel-button" type="button" onClick={toggleModal}>
