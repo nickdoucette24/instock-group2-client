667426fd41aecc4e5d94f82a7abe0a66241a1e13
Kevin Meisenbacher <BeastOfTheEast23@hotmail.ca>
2024-05-28 22:59:54 -0400

    Undid sorty-selectors hotfix for inventory row because it was causing layout issues

--- src/components/Sorter/Sorter.js
+++ src/components/Sorter/Sorter.js
@@ -1,34 +1,33 @@
 import sortIcon from '../../assets/Icons/sort-24px.svg';
 
 import './Sorter.scss';
-import '../WarehouseList/WarehouseList.scss';
 import '../InventoryItemRow/InventoryItemRow.scss'
 
 const Sorter = () => {
   return (
     <div className="sorter-container">
-      <div className='sorty-selectors'>
-        <div className='sorty-selectors__item'>
+      <div className='sorter-info'>
+        <div className='sorter-info__field'>
           <h4>INVENTORY ITEM</h4>
           <img src={sortIcon} alt='sort directory icon' />
         </div>
-        <div className='sorty-selectors__item'>
+        <div className='sorter-info__field'>
           <h4>CATEGORY</h4>
           <img src={sortIcon} alt='sort directory icon' />
         </div>
-        <div className='sorty-selectors__item'>
+        <div className='sorter-info__field'>
           <h4>STATUS</h4>
           <img src={sortIcon} alt='sort directory icon' />
         </div>
-        <div className='sorty-selectors__item'>
+        <div className='sorter-info__field'>
           <h4>QTY</h4>
           <img src={sortIcon} alt='sort directory icon' />
         </div>
-        <div className='sorty-selectors__item'>
+        <div className='sorter-info__field'>
           <h4>WAREHOUSE</h4>
           <img src={sortIcon} alt='sort directory icon' />
         </div>
-        <div className='sorty-selectors__item actions'>
+        <div className='sorter-info__field actions'>
           <h4>ACTIONS</h4>
         </div>
       </div>
